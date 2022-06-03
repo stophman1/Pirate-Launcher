bc91863f99ce2b30c5d7b71a1ad1844cfa555307
stophman1 <11683358+stophman1@users.noreply.github.com>
2022-06-03 12:22:40 -0500

    Updated Dependencies & Cleaned Up Code.

--- src/main/java/com/apeeling/piratelauncher/RequestManager.java
+++ src/main/java/com/apeeling/piratelauncher/RequestManager.java
@@ -12,7 +12,7 @@ import org.apache.commons.lang3.SystemUtils;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
 public class RequestManager {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //System.out.print(GetExec("https://download.tlopo.com/win32/patcher.json"));
     }
 
@@ -47,10 +47,9 @@ public class RequestManager {
         try (InputStream is = new URL(patcherURL).openStream();
              Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
             Gson gson = new Gson();
-            Map<String, Map<String, Object>> theMap = gson.fromJson(reader, Map.class);
-            Map<String, Object> treemap = theMap.get("files");
-            //System.out.println(treemap.keySet().toArray()[0]); //prints 1st file name
-            return treemap;
+            Map<String, Map<String, Object>> theMap;
+            theMap = gson.fromJson(reader, Map.class);
+            return theMap.get("files");
         }
 
 
@@ -59,39 +58,34 @@ public class RequestManager {
 
     public void DownloadMan(String server) throws IOException, NoSuchAlgorithmException {
         String osUsed = detectOS();
-        if (osUsed == "unsupported") {
+        if (osUsed.equals("unsupported")) {
             ErrorWindow dialog = new ErrorWindow("Your OS is not supported by this launcher.");
             dialog.pack();
             dialog.setVisible(true);
         }
         Map<String, Object> curFile = GetFiles(server);
         Object[] leArray = curFile.keySet().toArray();
-        Thread downThing = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < leArray.length; i++) {
-                    try {
-                        downFile(osUsed,
-                                curFile.get(leArray[i]).toString().substring(1, curFile.get(leArray[i]).toString().length() - 1).split(", ")[0].substring(5),
-                                (String) leArray[i],
-                                server,
-                                curFile.get(leArray[i]).toString().substring(1, curFile.get(leArray[i]).toString().length() - 1).split(", ")[1].substring(5));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                    }
+        Thread downThing = new Thread(() -> {
+            for (Object o : leArray) {
+                try {
+                    downFile(osUsed,
+                            curFile.get(o).toString().substring(1, curFile.get(o).toString().length() - 1).split(", ")[0].substring(5),
+                            (String) o,
+                            server,
+                            curFile.get(o).toString().substring(1, curFile.get(o).toString().length() - 1).split(", ")[1].substring(5));
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    e.printStackTrace();
                 }
-                ErrorWindow dialog = new ErrorWindow("Download complete. :- ]");
-                dialog.pack();
-                dialog.setVisible(true);
             }
+            ErrorWindow dialog = new ErrorWindow("Download complete. :- ]");
+            dialog.pack();
+            dialog.setVisible(true);
         });
         downThing.start();
     }
 
     public void downFile(String osUsed, String filePath, String file, String server, String hash) throws IOException, NoSuchAlgorithmException {
-        if (hashCompare(file, server, hash) == true) {
+        if (hashCompare(file, server, hash)) {
             //System.out.println("SAME HASH!!!");
             return;
         }
@@ -101,45 +95,42 @@ public class RequestManager {
         try (BufferedInputStream inputStream = new BufferedInputStream(new URL(downloadURL).openStream());
              FileOutputStream fileOut = new FileOutputStream(file + ".bz2")) //I forgor 💀 this isn't old Java :( I ain't going to rewrite this
         {
-            byte data[] = new byte[1024];
+            byte[] data = new byte[1024];
             int byteContent;
             while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
                 fileOut.write(data, 0, byteContent);
             }
         }
-        ;
         bz2Man(file);
     }
 
     public void bz2Man(String file) throws IOException {
         try (BZip2CompressorInputStream inputStream = new BZip2CompressorInputStream(new BufferedInputStream(new FileInputStream(file + ".bz2")));
-             FileOutputStream fileOut = new FileOutputStream(file);) {
-            byte data[] = new byte[1024];
+             FileOutputStream fileOut = new FileOutputStream(file)) {
+            byte[] data = new byte[1024];
             int byteContent;
             while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
                 fileOut.write(data, 0, byteContent);
             }
         }
-        ;
         new File(file + ".bz2").delete();
     }
 
     public String hashCheck(String file) throws IOException, NoSuchAlgorithmException {
         FileInputStream inputStream = new FileInputStream(file);
-        byte data[] = new byte[1024];
+        byte[] data = new byte[1024];
         int byteContent;
         MessageDigest digest = MessageDigest.getInstance("SHA256");
         while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
             digest.update(data, 0, byteContent);
         }
-        ;
         inputStream.close();
         byte[] bytes = digest.digest();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 4; i++) {
             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
         }
-        return sb.toString().substring(0, sb.toString().length() - 1);
+        return sb.substring(0, sb.toString().length() - 1);
     }
 
     public boolean hashCompare(String file, String Server, String hash) throws IOException, NoSuchAlgorithmException {
@@ -147,10 +138,7 @@ public class RequestManager {
         File dafile = new File(file);
         //System.out.println("hashCheck: "+hashCheck(file));
         //System.out.println("hash: "+hash);
-        if (dafile.exists() && hashCheck(file).equals(hash)) {
-            return true;
-        }
-        return false;
+        return dafile.exists() && hashCheck(file).equals(hash);
     }
 
     public String detectOS() {
@@ -178,8 +166,7 @@ public class RequestManager {
         http.setDoOutput(true);
         Map<String, String> request = new HashMap<>();
         request.put("username", username);
-        String pass = new String();
-        request.put("password", pass.valueOf(password));
+        request.put("password", String.valueOf(password));
         StringJoiner sj = new StringJoiner("&");
         for (Map.Entry<String, String> entry : request.entrySet())
             sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
@@ -195,7 +182,7 @@ public class RequestManager {
             os.close();
             BufferedReader input = new BufferedReader(new InputStreamReader(http.getInputStream()));
             Gson gson = new Gson();
-            Map<String, Map<String, Object>> theMap = gson.fromJson(input, Map.class);
+            Map theMap = gson.fromJson(input, Map.class);
             Double status = new Double(theMap.values().toArray()[0].toString());
             if (!status.equals(7.0) && !status.equals(3.0))
             {
